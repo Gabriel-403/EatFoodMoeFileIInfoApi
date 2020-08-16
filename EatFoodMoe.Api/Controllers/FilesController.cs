58fb93551678e7f2d6d22a94fdc69970a1da7661
chaosprogrammer <1499015923@qq.com>
2020-08-16 22:43:16 +0800

    添加项目文件。

---
+++ EatFoodMoe.Api/Controllers/FilesController.cs
@@ -0,0 +1,23 @@
+﻿using EatFoodMoe.Api.Entitles;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatFoodMoe.Api.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FilesController:ControllerBase
+    {
+        [HttpPost]
+        public IActionResult UpLoad([FromForm] EatFoodIFilesInfo eatFoodIFilesInfo) 
+        {
+
+            return Ok();
+        }
+
+
+    }
+}
---
+++ EatFoodMoe.Api/Entitles/EatFoodIFilesInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatFoodMoe.Api.Entitles
+{
+    public class EatFoodIFilesInfo
+    {
+        public string Title { get; set; }
+        public List<IFormFile> Files { get; set; }
+     
+
+
+}
+}
